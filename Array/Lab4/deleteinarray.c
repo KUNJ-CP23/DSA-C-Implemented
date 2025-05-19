c9329139b779ded07db47242650c57ea4e599ce3
Kunj Ramoliya <kunjramoliya367@gmail.com>
2025-05-19 23:25:57 +0530

    temp

--- Array/Lab4/deleteinarray.c
+++ Array/Lab4/deleteinarray.c
@@ -1,6 +1,5 @@
 // 25. Delete
 #include <stdio.h>
-
 void main() {
     int a[100], n, i, pos;
     printf("Enter size: ");
@@ -9,8 +8,9 @@ void main() {
         scanf("%d", &a[i]);
     printf("Enter position to delete: ");
     scanf("%d", &pos);
-    for(i = pos; i < n - 1; i++)
+    for(i = pos; i < n - 1; i++){
         a[i] = a[i + 1];
+    }
     n--;
     for(i = 0; i < n; i++)
         printf("%d ", a[i]);
--- Array/Lab4/deleteinsorted.c
+++ Array/Lab4/deleteinsorted.c
@@ -20,6 +20,7 @@ void main() {
             a[i] = a[i + 1];
         n--;
     }
-    for(i = 0; i < n; i++)
+    for(i = 0; i < n; i++){
         printf("%d ", a[i]);
+    }     
 }
