6ad9514dfb5012270f7de76c87b3abc5dfa562b2
Kunj Ramoliya <kunjramoliya367@gmail.com>
2025-05-22 15:11:44 +0530

    lab3

--- Array/Lab3/pascal.c
+++ Array/Lab3/pascal.c
@@ -1,13 +1,16 @@
 //22. Pascal
 #include <stdio.h>
+
 void main() {
-    int n, i, j, num;
+    int n, i, j, num, spc;
     printf("Enter rows: ");
     scanf("%d", &n);
     for(i = 0; i < n; i++) {
+        for(spc = 0; spc < n - i - 1; spc++)
+            printf("  ");
         num = 1;
         for(j = 0; j <= i; j++) {
-            printf("%d ", num);
+            printf("%4d", num);
             num = num * (i - j) / (j + 1);
         }
         printf("\n");
